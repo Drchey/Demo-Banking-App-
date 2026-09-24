18330b780067ae4ed6e360ae76898857cc47bded
drchey <derichey9@gmail.com>
2026-09-24 13:14:58 +0100

    add api route to transactions and accounts

--- src/main/java/com/richey/gtbankapp/controller/TransactionController.java
+++ src/main/java/com/richey/gtbankapp/controller/TransactionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/api/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
     private final TransactionServiceImpl transactionService;
